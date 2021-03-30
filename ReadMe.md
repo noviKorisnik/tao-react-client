3dedd0d51fd303823de97fc52c0b89b5ce273a07
Dejan laptop <dejan.katasic@gmail.com>
2021-03-30 21:11:00 +0200

    without service

--- ReadMe.md
+++ ReadMe.md
@@ -1,6 +1,8 @@
 [tao-react-client](https://github.com/noviKorisnik/tao-react-client)
 ___
 ### snapshot006
+## changed value provider
+To change the story a bit and detach from service, in **public** directory is now set json with complete Tao and value provider is directed there to fetch data with several helper methods to provide same output as one we got from service for the same key.
 ___
 | [Previous](https://github.com/noviKorisnik/tao-react-client/tree/snapshot005) | [Home](https://github.com/noviKorisnik/tao-react-client) | [Next](https://github.com/noviKorisnik/tao-react-client/tree/snapshot007) |
 | :-: | :-: | :-: |
--- tao-react-client/src/lib/valueProvider.js
+++ tao-react-client/src/lib/valueProvider.js
@@ -1,16 +1,69 @@
 import { getData, storeData, clearData } from './localStorage';
 
-const path = 'https://localhost:5001/';
+const path = '/Tao.json';
+
+const getTao = (t) => {
+    t.books.forEach(book => {
+        book.chapters = [];
+    });
+    return t;
+}
+const getBook = (t, code) => {
+    const book = t.books.find(b => b.code === code);
+    if (!book) return null;
+    book.chapters.forEach(chapter => {
+        chapter.paragraphs = [];
+    });
+    return book;
+}
+const getChapter = (t, code) => {
+    let chapter;
+    try {
+        const book = t
+            .books
+            .find(b => b.chapters.some(c => c.code === code));
+        chapter = t
+            .books
+            .find(b => b.chapters.some(c => c.code === code))
+            .chapters
+            .find(c => c.code === code);
+    } catch (e) {
+        return null;
+    }
+    if (!chapter) return null;
+    return chapter;
+}
 
 const provideData = async (key) => {
     let data = getData(key);
     if (data) {
         return data;
     }
-    //better check what if something goes wrong...
-    data = await (await fetch(path + key)).json();
-    storeData(key, data);
-    return data;
+    if (key) {
+        const [a, b, c] = key.split('/');
+        const t = await (await fetch(path)).json();
+        if (a.toLowerCase() === 'tao') {
+            if (b === undefined) {
+                data = getTao(t);
+            } else {
+                switch (b.toLowerCase()) {
+                    case 'book':
+                        data = getBook(t, c);
+                        break;
+                    case 'chapter':
+                        data = getChapter(t, c);
+                        break;
+                    default:
+                        break;
+                };
+            }
+        }
+    }
+    if (data) {
+        storeData(key, data);
+        return data;
+    }
+    return null;
 }
 
 export default provideData;
